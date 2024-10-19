3e9af9d317a271b352d0ce7310752a9a118eab12
Sobicus <maksymvasylenko88@gmail.com>
2024-10-19 12:21:31 +0200

    deploy prod tesÐt 4

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -7,7 +7,10 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('rocess.env.PORT', process.env.PORT)
+  const port = process.env.PORT ?? 3000
   app.setGlobalPrefix('api/v1')
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
+  console.log('post', port)
+  await app.listen(port);
 }
 bootstrap();
