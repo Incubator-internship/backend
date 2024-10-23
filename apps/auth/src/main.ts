c1b474f273a89559c3a513fed598bce08c583496
7erp1c <ul_tray@bk.ru>
2024-10-23 22:09:18 +0300

    add setting project

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { appSettings } from '../settings/app-settings';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('rocess.env.PORT', process.env.PORT)
-  const port = process.env.PORT ?? 3000
-  app.setGlobalPrefix('api/v1')
+  console.log('process.env.PORT', appSettings.api.AUTH_PORT);
+  const port = appSettings.api.AUTH_PORT ?? 3000;
+  app.setGlobalPrefix('api/v1');
   //await app.listen(process.env.PORT ?? 3000);
-  console.log('post', port)
+  console.log('post', port);
   await app.listen(port);
 }
 bootstrap();
