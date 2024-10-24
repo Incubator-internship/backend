5130a68fbdc473c653efa35e381a8fc3bf6a8ff6
Sobicus <maksymvasylenko88@gmail.com>
2024-10-24 13:03:44 +0200

    add prisma to our project

---
+++ apps/auth/prisma/migrations/20241023194803_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "UserMan" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserMan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserMan_email_key" ON "UserMan"("email");
