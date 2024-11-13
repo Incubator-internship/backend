f88368b7f75c5e3886a81cdacb78c13df8d15934
Sobicus <maksymvasylenko88@gmail.com>
2024-11-13 14:41:56 +0100

    port

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -8,7 +8,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('process.env.PORT', appSettings.api.AUTH_PORT);
-  const port = appSettings.api.AUTH_PORT ?? 3000;
+  //const port = appSettings.api.AUTH_PORT ?? 3000;
+  const port = process.env.PORT ?? 3000;
   app.setGlobalPrefix('api/v1');
   //await app.listen(process.env.PORT ?? 3000);
   console.log('post', port);
