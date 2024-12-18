ebd5f89b138e7e07fe05e4b78805388b693a9113
Sobicus <maksymvasylenko88@gmail.com>
2024-12-18 11:20:40 +0100

    create post with proces v0.2

--- apps/files/src/main.ts
+++ apps/files/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { FilesModule } from './files.module';
+import { applyAppFileSettings } from '../settings/apply-app-file-setting';
+import { filesSettings } from '../settings/file-configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(FilesModule);
-  await app.listen(process.env.port ?? 3000);
+  applyAppFileSettings(app);
+
+  console.log('appSettings.api.FILE_PORT', filesSettings.api.FILE_PORT);
+  const port = filesSettings.api.FILE_PORT ?? 5101;
+
+  console.log('file port', port);
+  await app.listen(port ?? 3000);
 }
 bootstrap();
