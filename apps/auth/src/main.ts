eecb2ace20d506192c8e96234a58c63106793973
Sobicus <maksymvasylenko88@gmail.com>
2024-12-26 11:28:24 +0100

    add test.env v1.12

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 import { appSettings } from '../settings/configuration';
 import { applyAppSettings } from '../settings/apply-app-setting';
 import { filesSettings } from '../../files/settings/file-configuration';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   applyAppSettings(app);
 
-  console.log(' filesSettings.api.PORT;', filesSettings.api.PORT);
-  console.log(' filesSettings.api.AUTH_PORT;', filesSettings.api.AUTH_PORT);
+  console.log(' filesSettings.api.PORT', filesSettings.api.PORT);
+  console.log(' filesSettings.api.AUTH_PORT', filesSettings.api.AUTH_PORT);
   console.log('appSettings.api.AUTH_PORT', appSettings.api.AUTH_PORT);
   console.log('process.env.PORT', process.env.PORT);
   //app.setGlobalPrefix('api/v1');
@@ -19,6 +22,6 @@ async function bootstrap() {
   //await app.listen(process.env.PORT ?? 3000);
   console.log('auth port', port);
   await app.listen(port);
-  console.log(`Files microservice is running on ${port}`);
+  console.log(`Auth microservice is running on ${port}`);
 }
 bootstrap();
