dd65eb634670916293193a72b71c9973df7d01f9
Ali <4e5.kn@mail.ru>
2025-07-22 23:58:17 +0300

    fix: paypal

--- apps/auth/src/payments/application/paypal.client.service.ts
+++ apps/auth/src/payments/application/paypal.client.service.ts
@@ -1,10 +1,9 @@
-// src/paypal/paypal.service.ts
 import { Injectable } from '@nestjs/common';
-import checkoutNodeJssdk from '@paypal/checkout-server-sdk';
+import * as paypal from '@paypal/checkout-server-sdk';
 
 @Injectable()
 export class PaypalClientService {
-  private client: checkoutNodeJssdk.core.PayPalHttpClient;
+  private client: paypal.core.PayPalHttpClient;
 
   constructor() {
     const clientId =
@@ -12,14 +11,14 @@ export class PaypalClientService {
     const clientSecret =
       'EKsJv3PrIvXA_wUbBXKE8dFcl1UiCgTl6ZMtsGC1g2E4cuHvr29fn-FoZ4almSiHUwQy6KJGT9Grzy7m';
 
-    const environment = new checkoutNodeJssdk.core.SandboxEnvironment(
+    const environment = new paypal.core.SandboxEnvironment(
       clientId,
       clientSecret,
     );
-    this.client = new checkoutNodeJssdk.core.PayPalHttpClient(environment);
+    this.client = new paypal.core.PayPalHttpClient(environment);
   }
 
-  getClient(): checkoutNodeJssdk.core.PayPalHttpClient {
+  getClient(): paypal.core.PayPalHttpClient {
     return this.client;
   }
 }
